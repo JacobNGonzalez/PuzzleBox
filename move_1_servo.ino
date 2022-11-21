cc43692c9f70c7181264f04cd6167334d58ba980
Jake <jng@gatech.edu>
2022-11-21 16:40:53 -0500

    adding ino file

--- move_1_servo.ino
+++ move_1_servo.ino
@@ -1,9 +1,47 @@
+#include <Servo.h>
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+int pos = 0;
+Servo myservo;
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.print("running now");
+  myservo.attach(9);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // Show the state of pushbutton on serial monitor
+  Serial.println(buttonState);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    for (pos = 0; pos <= 180; pos += 1) {
+      myservo.write(pos);
+      delay(15);
+    }
 
+    for (pos = 180; pos >= 0; pos -= 1) {
+      myservo.write(pos);
+      delay(15);
+    }
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+  // Added the delay so that we can see the output of button
+  delay(100);
 }
