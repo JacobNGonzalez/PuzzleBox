33adbe1995e5a5bb3314ab0f8ddac4387d9eaaad
Jake <jng@gatech.edu>
2022-12-07 15:42:23 -0500

    more organizing

--- move_1_servo.ino
+++ move_1_servo.ino
@@ -3,13 +3,13 @@
 #include <Keypad.h>
 //// ###########################
 
-//// ##### Initial Constants/Variables #####
+//// ##### Initial Variables #####
   //// |||||||| Hardware Specific |||||||
   //// LED variables
-    const int ledPin =  13;      // the number of the LED pin
+    const int led_green =  13; // pin number of green LED
 
   //// Button Variables
-    const int buttonPin = 12;     // the number of the pushbutton pin
+    const int btn_green = 12; // pin number of green pushbutton
 
   //// Keypad Variables
     const int ROW_NUM = 4; // num of rows rows on keypad
@@ -29,6 +29,7 @@
     int loc_forward = 180; // forward postion for continuous servos 
     int loc_reverse = 0; // reverse postion for continuous servos 
     Servo srv_podLift; // declare servo for the pod lift mechanism
+    int pin_srv_podLift = 11; // pin that pod lift servo will attach to
     Servo srv_rightLock; // declare servo for the right flap locking mechanism
     Servo srv_drawerLatch; // declare servo for the drawer latch mechanism
 
@@ -38,141 +39,96 @@
     String input_password; // actual password that the user input on the pad
     bool correct_pw = false; // whether the correct pw has been entered and confirmed
 
-
-
-int buttonState = 0; //pushbutton status   
+ 
 // setup function running at the beginning of the sketch
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // start serial monitor
+  Serial.print("setup running now");
+  input_password.reserve(10); // set memory reserve for password
 
-  input_password.reserve(10);
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  Serial.print("sketch running now");
-  myservo.attach(11);
-  myservo.write(90);
+  pinMode(led_green, OUTPUT); // initialize the LED pin as an output:
+  pinMode(btn_green, INPUT); // initialize the pushbutton pin as an input
 
-  delay(5000);
+  srv_podLift.attach(pin_srv_podLift); // attach the servo to pin
+  srv_podLift.write(loc_neutral); // move servo to neutral
+  delay(3000);
 }
 
 // main loop function
 void loop() {
 
-  myservo.write(180);
-  Serial.println("spinning servo forward - move to 180");
-  delay(1000);
-  myservo.write(90);
-  Serial.println("stopping servo - move to to 90");
-  delay(5000);
-  myservo.write(0);
-  Serial.println("spinning servo in reverse - move to to 0");
-  delay(1000);
-  myservo.write(90);
-  Serial.println("stopping servo - move to to 90");
-  delay(5000);
-  //  char key = keypad.getKey();
-  // if (key){
-  //   Serial.println(key);
 
-  //     if(key == '*') {
-  //       input_password = ""; // clear input password
-  //     } else if(key == '#') {
-  //     if(password == input_password) {
-  //       Serial.println("password is correct");
-  //       // turn LED on:
-  //       digitalWrite(ledPin, HIGH);
-  //       for (pos = 0; pos <= 90; pos += 1) {
-  //         myservo.write(pos);
-  //         delay(15);
-  //       }
-  //       delay(30);
-  //       for (pos = 90; pos >= 0; pos -= 1) {
-  //         myservo.write(pos);
-  //         delay(15);
-  //       }
-  //       digitalWrite(ledPin, LOW);
-  //       delay(100);
-  //     } else {
-  //       Serial.println("password is incorrect, try again");
-  //       // turn LED off:
-  //       digitalWrite(ledPin, LOW);
-  //       delay(100);
-  //     }
-
-  //     input_password = ""; // clear input password
-  //     } else {
-  //        input_password += key; // append new character to input password string
-  //     }
-  // }
-
-
-//_____________________________________________________________________________
-
-
-      // // read the state of the pushbutton value:
-      // buttonState = digitalRead(buttonPin);
-      // // Show the state of pushbutton on serial monitor
-      // Serial.println(buttonState);
-      // // check if the pushbutton is pressed.
-      // // if it is, the buttonState is HIGH:
-      // // if (buttonState == HIGH) {
-      //   if (correct_pw == true){
-      //   // turn LED on:
-      //   digitalWrite(ledPin, HIGH);
-      //   for (pos = 0; pos <= 90; pos += 1) {
-      //     myservo.write(pos);
-      //     delay(15);
-      //   }
-      //   delay(30);
-      //   for (pos = 90; pos >= 0; pos -= 1) {
-      //     myservo.write(pos);
-      //     delay(15);
-      //   }
-      //   digitalWrite(ledPin, LOW);
-      //   correct_pw = false;
-      // } else {
-      //   // turn LED off:
-      //   digitalWrite(ledPin, LOW);
-      // }
-      // // Added the delay so that we can see the output of button
-      // delay(100);
-} 
+ }
 
 // ################################################
-// all puzzle interaction functions below
 
-void simonSays() {
-  // puzzle to match RGBY button-press to pattern shown on LEDs
+//// ##### PUZZLE FUNCTIONS #####
+  // functions for puzzle interactions go below
 
-}
+  void simonSays() {
+    // puzzle to match RGBY button-press to pattern shown on LEDs
+
+  }
+
 
 // #################################################
 
-// #################################################
-// all box mechanical functions below
 
-void liftKeypod() {
+
+//// ##### MECHANICAL FUNCTIONS #####
+  // functions for box mechanichal movements go below
+
   // move the continuous servo to raise/lower the keypod cylinder
+  void liftKeypod() {
+    char key = keypad.getKey();
+    if (key){
+      Serial.println(key);
+      if(key == '*') {
+        input_password = ""; // clear input password
+      } else if(key == '#') {
+      if(password == input_password) {
+        Serial.println("password is correct");
+        // turn LED on:
+        digitalWrite(led_green, HIGH);
+        delay(3000);
+        digitalWrite(led_green, LOW);
+        delay(100);
+      } else {
+        Serial.println("password is incorrect, try again");
+        // turn LED off:
+        digitalWrite(led_green, LOW);
+        delay(100);
+      }
 
-}
+      input_password = ""; // clear input password
+      } else {
+        input_password += key; // append new character to input password string
+      }
+    }
+  }
 
-// function to test the movement of continuous servos
-// loop this when 0'ing out the potentiometer on one
-void testContinuousServo(Servo servoName) {
-  servoName.write(180);
-  Serial.println("spinning servo forward - move to 180");
-  delay(1000);
-  servoName.write(90);
-  Serial.println("stopping servo - move to to 90");
-  delay(5000);
-  servoName.write(0);
-  Serial.println("spinning servo in reverse - move to to 0");
-  delay(1000);
-  servoName.write(90);
-  Serial.println("stopping servo - move to to 90");
-  delay(5000);
-}
 
+// #################################################
+
+
+//// ##### DEVELOPMENT FUNCTIONS #####
+  // any functions below are for development, and can be removed to condense the sketch
+
+  // function to test the movement of continuous servos
+  // loop this when 0'ing out the potentiometer on one
+  void testContinuousServo(Servo servoName) {
+    servoName.write(180);
+    Serial.println("spinning servo forward - move to 180");
+    delay(1000);
+    servoName.write(90);
+    Serial.println("stopping servo - move to to 90");
+    delay(5000);
+    servoName.write(0);
+    Serial.println("spinning servo in reverse - move to to 0");
+    delay(1000);
+    servoName.write(90);
+    Serial.println("stopping servo - move to to 90");
+    delay(5000);
+  }
+  
 // #################################################
